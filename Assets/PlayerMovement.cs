deb8ecf0183eef65872ebb02883305606600a969
Gabe Wilmoth <35712811+GabeWilmoth@users.noreply.github.com>
2019-03-28 22:22:24 -0400

    Added Triggers and Animation for MVP
    Added in a simple box trigger, allowing the Player object to collide and teleport to a new location.
    (The hit on the trigger can be changed depending on what player we have i.e one class may not be able to hit the trigger while another can)
    Also added in the animation for Idle, Running, and Jumping.

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -6,6 +6,7 @@ public class PlayerMovement : MonoBehaviour
 {
 
     public CharacterController2D controller;
+    public Animator animator;
 
     float horizontalMove = 0f;
     public float runSpeed = 40f;
@@ -16,10 +17,18 @@ public class PlayerMovement : MonoBehaviour
     {
         horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
 
+        animator.SetFloat("Speed", Mathf.Abs(horizontalMove));
+
         if (Input.GetButtonDown("Jump"))
         {
             jump = true;
+            animator.SetBool("IsJumping", true);
         }
+    }
+
+    public void OnLanding()
+    {
+        animator.SetBool("IsJumping", false);
     }
 
     private void FixedUpdate()
