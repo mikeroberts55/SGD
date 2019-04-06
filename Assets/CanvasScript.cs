10858d46e051e1cf57909ee26baa25b9a7ab5662
Gabe Wilmoth <35712811+GabeWilmoth@users.noreply.github.com>
2019-04-05 22:19:38 -0400

    Added Ranger Teleport Ability
    Ranger Tag for new player and Text display upon entering trigger as well as added fuctionality for that text.

---
+++ Assets/CanvasScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CanvasScript : MonoBehaviour
+{
+    public Text canvasText1;
+
+    private void OnTriggerStay2D(Collider2D other)
+    {
+        canvasText1.enabled = false;
+
+        if (other.CompareTag("ranger"))
+        {
+            canvasText1.enabled = true;
+        }
+    }
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("ranger"))
+        {
+            canvasText1.enabled = false;
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("ranger"))
+        {
+            canvasText1.enabled = true;
+        }
+    }
+
+}
